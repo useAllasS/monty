34644300eb40e162c040ee46a92363aab240c78b
Nita Youssef <nitayoussef3@gmail.com>
2023-12-16 02:58:01 -0800

    3321

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-C - Stacks, Queues - LIFO, FIFO
+0x19. C - Stacks, Queues - LIFO, FIFO
--- monty.h
+++ monty.h
@@ -1,18 +1,7 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <dirent.h>
-
-#define EXIT_FAILURE 1
-#define EXIT_SUCCESS 0
-#define MAX_LENGTH 256
-
+#include <stddef.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -24,9 +13,9 @@
  */
 typedef struct stack_s
 {
-		int n;
-		struct stack_s *prev;
-		struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -35,92 +24,71 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO 
  */
 typedef struct instruction_s
 {
-		char *opcode;
-		void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-
+#define INSTRUCTIONS { \
+		{"push", push},\
+		{"pall", pall},\
+		{"pint", pint},\
+		{"pop", pop},\
+		{"swap", swap},\
+		{"nop", nop},\
+		{"div", _div},\
+		{"mul", _mul},\
+		{"add", _add},\
+		{"sub", _sub},\
+		{"mod", mod},\
+		{"pchar", pchar},\
+		{"pstr", pstr},\
+		{"rotl", rotl},\
+		{"rotr", rotr},\
+		{NULL, NULL} \
+	}
 /**
- * struct ouafivar - global variabels
- * @val_read: the value readed
- * @line_read: the line readed
- * @file_read: the stream of the file readed
- * @type: define type of storing set stack as default
- * Description: the global variables that we used
- */
-typedef struct omarvar
+* struct help - argument for the current opcode
+* @data_struct: stack mode, stack (default) and queue
+* @argument: the arguments of the string
+*
+* Description: global structure used to pass data around the functions easily
+*/
+typedef struct help
 {
-	int val_read;
-	char *line_read;
-	FILE *file_read;
-	char *type;
-} glob;
+	int data_struct;
+	char *argument;
+} help;
+help global;
 
-extern glob var1;
+extern int status;
 
+void push(stack_t **stack, unsigned int line_cnt);
+void pall(stack_t **stack, unsigned int line_cnt);
+void pint(stack_t **stack, unsigned int line_cnt);
+void swap(stack_t **stack, unsigned int line_cnt);
+void pop(stack_t **stack, unsigned int line_cnt);
+void nop(stack_t **stack, unsigned int line_cnt);
+void _div(stack_t **stack, unsigned int line_cnt);
+void _add(stack_t **stack, unsigned int line_cnt);
+void _sub(stack_t **stack, unsigned int line_cnt);
+void _mul(stack_t **stack, unsigned int line_cnt);
+void mod(stack_t **stack, unsigned int line_cnt);
+void pchar(stack_t **stack, unsigned int line_cnt);
+void pstr(stack_t **stack, unsigned int line_cnt);
+void rotl(stack_t **stack, unsigned int line_count);
+void rotr(stack_t **stack, unsigned int line_count);
+void opcode(stack_t **stack, char *str, unsigned int line_cnt);
 
-stack_t *push_stack(stack_t **head, int n);
-stack_t *push_queue(stack_t **head, int n);
-void error_memory(stack_t **stack, unsigned int line_num);
+int is_digit(char *string);
+int isnumber(char *str);
 
-void free_dlistint(stack_t *head);
+stack_t *add_node(stack_t **stack, const int n);
+stack_t *queue_node(stack_t **stack, const int n);
+void free_stack(stack_t *stack);
+size_t print_stack(const stack_t *stack);
 
-void getinstruction(instruction_t *instructions);
-
-void push(stack_t **stack, unsigned int line_number);
-void error_push(stack_t **stack, unsigned int line_num);
-
-void pall(stack_t **stack, unsigned int line_number);
-void error_undefined(stack_t **stack, unsigned int line_num);
-
-void pint(stack_t **stack, unsigned int line_number);
-void error_pint(stack_t **stack, unsigned int line_num);
-
-void pop(stack_t **stack, unsigned int line_number);
-void error_pop(stack_t **stack, unsigned int line_num);
-
-void swap(stack_t **stack, unsigned int line_number);
-void error_swap(stack_t **stack, unsigned int line_num);
-
-void add(stack_t **stack, unsigned int line_number);
-void error_add(stack_t **stack, unsigned int line_num);
-
-void nop(stack_t **stack, unsigned int line_number);
-
-void sub(stack_t **stack, unsigned int line_number);
-void error_sub(stack_t **stack, unsigned int line_num);
-
-void _div(stack_t **stack, unsigned int line_number);
-void error_div(stack_t **stack, unsigned int line_num);
-
-void mul(stack_t **stack, unsigned int line_number);
-void error_mul(stack_t **stack, unsigned int line_num);
-
-void mod(stack_t **stack, unsigned int line_number);
-void error_mod(stack_t **stack, unsigned int line_num);
-
-void error_math(stack_t **stack, unsigned int line_num);
-
-void put_char(stack_t **stack, unsigned int line_number);
-void error_put_char_empty(stack_t **stack, unsigned int line_num);
-void error_not_char(stack_t **stack, unsigned int line_num);
-
-void pstr(stack_t **stack, unsigned int line_number);
-
-void rotl(stack_t **stack, unsigned int line_number);
-
-void rotr(stack_t **stack, unsigned int line_number);
-
-void queue(stack_t **stack, unsigned int line_number);
-void stack(stack_t **stack, unsigned int line_number);
-
-int _isdigit(int c);
-int _isinteger(char *str);
-char *discardSpaces(char *old_line);
-
-
-#endif
+#endif /* MONTY_H */
